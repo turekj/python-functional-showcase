dd80ad489a43df5895f0c6c3f1a6517b170a7ef1
Jakub Turek <jkbturek@gmail.com>
2016-10-02 15:36:03 +0200

    Implemented fetching repositories starred by user

--- test/test_github.py
+++ test/test_github.py
@@ -20,3 +20,16 @@ class TestGithub(unittest.TestCase):
         self.assertEqual({'login': 'octocat'}, result)
         rx_get_json.assert_called_once_with(
             'https://api.github.com/users/octocat')
+
+    @patch('service.github.rx_get_json')
+    def test_starred_repositories(self, rx_get_json):
+        """
+        Test that rx_user_details issues correct request.
+        """
+        rx_get_json.return_value = [{'id': 123}]
+
+        result = self.sut.rx_starred_repositories('octocat')
+
+        self.assertEqual([{'id': 123}], result)
+        rx_get_json.assert_called_once_with(
+            'https://api.github.com/users/octocat/starred')
