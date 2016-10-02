e459cc12a49b97974d325c8858a82cd6499178bf
Jakub Turek <jkbturek@gmail.com>
2016-10-02 17:45:51 +0200

    Implemented hireable finder

--- logic/hireable.py
+++ logic/hireable.py
@@ -1,3 +1,44 @@
+from collections import OrderedDict
+
+import funcy
+import rx
+import rx.testing.dump
+
+from service.github import GithubService
+
+
 class HireableFinder:
-    def find_hireable(self, initial_username):
-        pass
+    """
+    Finds hireable Github users.
+
+    :param GithubService github_service: Github service
+    """
+    def __init__(self, github_service):
+        self.github = github_service
+
+    def find_hireable(self, initial_user):
+        return self.github.rx_starred_repositories(initial_user) \
+            .map(funcy.partial(funcy.lpluck, 'name')) \
+            .flat_map_latest(self.__contributors) \
+            .map(funcy.rpartial(funcy.without, initial_user)) \
+            .map(self.__sort) \
+            .flat_map_latest(self.__hireable)
+
+    def __contributors(self, repos):
+        return rx.Observable.concat(
+            list(map(self.github.rx_contributors, repos))) \
+            .buffer_with_count(len(repos)) \
+            .map(funcy.flatten) \
+            .map(funcy.partial(funcy.pluck, 'login'))
+
+    def __sort(self, contributors):
+        return OrderedDict(
+            sorted(funcy.count_by(funcy.identity, contributors).items(),
+                   key=lambda i: i[1], reverse=True)).keys()
+
+    def __hireable(self, logins):
+        return rx.Observable.concat(
+            list(map(self.github.rx_user_details, logins))) \
+            .buffer_with_count(len(logins)) \
+            .map(funcy.partial(funcy.where, hireable=True)) \
+            .map(funcy.partial(funcy.lpluck, 'login'))
---
+++ test/test_hireable_finder.py
@@ -0,0 +1,89 @@
+import unittest
+from unittest.mock import MagicMock, call
+
+import rx
+
+from logic.hireable import HireableFinder
+
+
+class TestHireableFinder(unittest.TestCase):
+    def setUp(self):
+        self.users = {
+            'manisero': {
+                'id': 123, 'login': 'manisero', 'hireable': None
+            },
+            'octocat': {
+                'id': 567, 'login': 'octocat', 'hireable': True,
+            },
+            'ninja_coder': {
+                'id': 1337, 'login': 'ninja_coder', 'hireable': True,
+            },
+            'lame_guy': {
+                'id': 0, 'login': 'lame_guy', 'hireable': False
+            },
+            'turekj': {
+                'id': 666, 'login': 'turekj', 'hireable': True
+            },
+        }
+        self.repositories = [
+            {'id': 1, 'name': 'lame_guy/not-worth-a-penny-software'},
+            {'id': 2, 'name': 'overpaid-experts/hello-world-printer'},
+            {'id': 3, 'name': 'anonymous/facebook-scanner'},
+        ]
+        self.contributors = {
+            'lame_guy/not-worth-a-penny-software': [
+                {'id': 0, 'login': 'lame_guy'},
+                {'id': 666, 'login': 'turekj'}
+            ],
+            'overpaid-experts/hello-world-printer': [
+                {'id': 567, 'login': 'octocat'},
+                {'id': 123, 'login': 'manisero'},
+                {'id': 1337, 'login': 'ninja_coder'}
+            ],
+            'anonymous/facebook-scanner': [
+                {'id': 0, 'login': 'lame_guy'},
+                {'id': 1337, 'login': 'ninja_coder'}
+            ]
+        }
+
+        def get_user(user):
+            return rx.Observable.from_([self.users[user]])
+
+        def get_repositories(_):
+            return rx.Observable.from_([self.repositories])
+
+        def get_contributors(repo):
+            return rx.Observable.from_([self.contributors[repo]])
+
+        self.github_service = MagicMock()
+        self.github_service.rx_user_details.side_effect = get_user
+        self.github_service.rx_starred_repositories.side_effect = \
+            get_repositories
+        self.github_service.rx_contributors.side_effect = get_contributors
+
+        self.sut = HireableFinder(self.github_service)
+
+    def test_find_hireable_method(self):
+        """
+        Test that find_hireable method returns correct results.
+        """
+        result = None
+
+        def on_next(r):
+            nonlocal result
+            result = r
+
+        self.sut.find_hireable('turekj').subscribe(on_next=on_next)
+
+        self.assertEqual(['ninja_coder', 'octocat'], result)
+        self.github_service.rx_starred_repositories.assert_called_once_with(
+            'turekj')
+        self.github_service.rx_user_details.assert_has_calls([
+            call('lame_guy'), call('octocat'), call('manisero'),
+            call('ninja_coder')
+        ], any_order=True)
+        self.github_service.rx_contributors.assert_has_calls([
+            call('lame_guy/not-worth-a-penny-software'),
+            call('overpaid-experts/hello-world-printer'),
+            call('anonymous/facebook-scanner'),
+        ], any_order=True)
